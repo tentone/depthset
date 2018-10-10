0ccb5acbfbf8c0585218c5232e1170158f5cbcf8
Tentone <tentone@outlook.com>
2018-10-10 16:17:09 +0100

    List files

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
 "use strict";
 
-//TODO <ADD CODE HERE>
+var input = "./data/";
+
+var fs = require("fs");
+var files = fs.readdirSync(input);
+
+for(var i = 0; i < files.length; i++)
+{
+	console.log(files[i]);
+}
+
+function writeFile(arraybuffer, fname)
+{
+	var buffer = fromArrayBuffer(arraybuffer);
+	var stream = fs.createWriteStream(fname);
+	stream.write(buffer);
+	stream.end();
+}
+
+function fromArrayBuffer(array)
+{
+	var buffer = new Buffer(array.byteLength);
+	var view = new Uint8Array(array);
+	
+	for(var i = 0; i < buffer.length; i++)
+	{
+		buffer[i] = view[i];
+	}
+
+	return buffer;
+}
